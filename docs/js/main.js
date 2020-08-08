5e48749a4216654b8cc66a05b3b482e3cdf821ce
Alexander <folickthebest@gmail.com>
2020-08-09 02:04:07 +0300

    create mobile menu + anchor script

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,15 @@
 $(function () {
+  $(".hamburger").on("click", function (e) {
+    $(this).toggleClass("is-active"),
+      e.preventDefault(),
+      $(".header__menu-list").toggleClass("is-active")
+  });
+  $('a[href^="#"]').click(function () {
+    let anchor = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(anchor).offset().top
+    }, 600);
+  });
   $('.about__slider').slick({
     prevArrow: '<button type="button" class="slick-prev slick-arrow"><img src="images/left-arrow.svg" alt="left"></button>',
     nextArrow: '<button type="button" class="slick-next slick-arrow"><img src="images/right-arrow.svg" alt="right"></button>',
